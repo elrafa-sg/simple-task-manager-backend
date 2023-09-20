6358c4297e721775561c23ad1d7681d28675943c
Rafael Gomes <silveiragomesr@gmail.com>
2023-09-20 20:33:38 -0300

    fix: remove relação redundante entre usuario e tarefa

--- models/usuario.js
+++ models/usuario.js
@@ -1,15 +1,13 @@
 'use strict';
-const { Model } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+const { Model, DataTypes } = require('sequelize');
+module.exports = (sequelize) => {
   class Usuario extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      Usuario.hasMany(models.Tarefas);
-    }
+    static associate(models) {}
   }
   Usuario.init(
     {
