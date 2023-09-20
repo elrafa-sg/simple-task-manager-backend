da648a305eb94dc48d0fe1a8860293c4aabf3159
Rafael Gomes <silveiragomesr@gmail.com>
2023-09-20 20:31:53 -0300

    feat: cria utilitario para gerenciar a criação/validação dos tokens jwt

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
 
 export const API_PORT = process.env.PORT || 4000
 export const API_HOST = process.env.HOST || 'http://localhost'
+
+
+export const ACCESS_EXPIRES = '1d'
+export const ACCESS_SECRET = 'quarentaedois'
