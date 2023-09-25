57f2a5ab699d2449140bee8aac92e092cd2ef289
Rafael Gomes <silveiragomesr@gmail.com>
2023-09-24 21:29:55 -0300

    chore: habilita cors para facilitar o desenvolvimento

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express'
+import cors from 'cors'
 import router from './routers'
 
 const app = express()
+
+app.use(cors({
+    origin: '*',
+    methods: ['*']
+}))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
