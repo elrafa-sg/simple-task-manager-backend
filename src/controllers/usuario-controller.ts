c544d19d8e2912587ae7bb60fa1ca15147d3c1d4
Rafael Gomes <silveiragomesr@gmail.com>
2023-09-27 23:15:36 -0300

    refactor: melhora tratamento de erros no login e crud de tarefas

--- src/controllers/usuario-controller.ts
+++ src/controllers/usuario-controller.ts
@@ -5,30 +5,32 @@ import { generateTokens, } from '../helpers/auth'
 import { sequelize } from '../../models'
 
 export class UsuarioController {
-    public async login (req: any, res: any) {
+    public login (req: any, res: any) {
         const { email, senha } = req.body
 
         const usuarioDao = Usuario(sequelize)
 
         try {
-            const usuario = await usuarioDao.findOne({
+            usuarioDao.findOne({
                 where: {
                     email: email,
                     senha: senha
                 }
             })
+                .then(usuario => {
+                    if (usuario == null) {
+                        return res.status(404).json({ message: 'Usuário ou senha inválidos!' })
+                    }
 
-            if (usuario == null) {
-                return res.status(404).json('Usuário ou senha inválidos!')
-            }
-
-            return res.status(200).json({
-                access_token: generateTokens({
-                    id: usuario?.dataValues.id
+                    return res.status(200).json({
+                        access_token: generateTokens({
+                            id: usuario?.dataValues.id
+                        })
+                    })
                 })
-            })
         } catch (error: any) {
-            return res.status(500).json(error)
+            console.warn('erro', error)
+            return res.status(500).json({ message: 'Ocorreu um erro ao processar sua requisição. Tente novamente mais tarde!' })
         }
     }
 
